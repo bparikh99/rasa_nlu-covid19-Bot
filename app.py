ba53ce90454e001bd5a057a6427eb7cc6045a2fe
bparikh99 <59947941+bparikh99@users.noreply.github.com>
2020-04-23 13:49:33 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,29 +1,39 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 14 16:23:04 2020
 
-@author: himanshimehta
-"""
-
-from bs4 import BeautifulSoup
-import requests
-import pandas as pd
 import json
-source=requests.get('https://api.covid19india.org/v2/state_district_wise.json').json()
-# print(source)X
-user_input=input("Enter State or District::")
-lst_num=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
-for i in range(len(source)):
-    if(source[i]['state']==user_input):
-        print("yes please enter your district")
-        user=input("Enter State or District::")
-        lst=source[i]['districtData']
-for j in range(len(lst)):
-    if(lst[j]['district']==user):
-        print("In your district {} total {} confirm cases ".format(user,lst[j]['confirmed']))
-
-       
-
-# x=df.loc[lambda df: df['Name of State / UT']==user_input]
-# print(x)
-# print("Total Cases =="+x['Total Confirmed cases (Including 76 foreign Nationals)'])
+import requests
+def cases(state,district):
+    global lst
+    source=requests.get('https://api.covid19india.org/v2/state_district_wise.json').json()
+   
+    for i in range(len(source)):
+        if(source[i]['state']==state):
+            lst=source[i]['districtData']
+            for j in range(len(lst)):
+                if(lst[j]['district']==district):
+                    ans=lst[j]['confirmed']
+                    #print(ans)
+                    return ans
+                
+def statewise(state_code):
+    url = "https://covid19india.p.rapidapi.com/getStateData"+'/'+state_code
+    
+    headers = {
+        'x-rapidapi-host': "covid19india.p.rapidapi.com",
+        'x-rapidapi-key': "216dd782cdmsh7f86900ecb6882dp14269fjsndf6ecc7de5a9"
+        }
+    
+    r = requests.request("GET", url, headers=headers).json()
+    x=r['response']['confirmed']
+    return x
+#cases('Kerala','Kollam')
+def state_data():
+    
+    lst=pd.read_html('https://www.mohfw.gov.in/')
+    dfs=lst[0]
+    dfs.drop(columns=['S. No.'],inplace=True)
+    dfs=dfs.rename(columns={'Name of State / UT':'State','Total Confirmed cases (Including 76 foreign Nationals)':'Cases',
+                         'Cured/Discharged/Migrated':'Cured','Death':'Death'})
+    dfs.replace("Nagaland#","Nagaland",inplace=True)
+    dfs.drop(index=[33,34,35],inplace=True)
+    data=dfs[dfs['Cases'].astype(str).astype(int) > 300]
+    return data
